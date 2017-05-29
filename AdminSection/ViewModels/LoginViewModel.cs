29941f1a95886babca7f519021f4cf06c5fcef2f
Евгений Комиссаров <komissarovea@gmail.com>
2017-05-29 12:28:29 +0300

    Add login function

---
+++ AdminSection/ViewModels/LoginViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminSection.ViewModels
+{
+    public class LoginViewModel
+    {
+        //[Required]
+        //[Display(Name = "E-mail")]
+        //public string Email { get; set; }
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember?")]
+        public bool RememberMe { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
